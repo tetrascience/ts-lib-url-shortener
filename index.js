260ebaf76469045024474098a3ca49752a547e8b
Michal Mazur <mmazur@tetrascience.com>
2019-11-15 14:17:47 -0500

    Lambda setup

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 const setupUrlShortener = require("./src/setupUrlShortener");
-const UrlShortener = require("./src/UrlShortener");
 
-module.exports = { setupUrlShortener, UrlShortener };
+exports.handler = async function(event) {
+  const response = {
+    statusCode: 200,
+  };
+  const { urlShortener } = await setupUrlShortener({
+    dbOpts: {
+      url: "FILL",
+      dbName: "FILL",
+      collectionName: "FILL"
+    },
+    shortenerOpts: { domain: "FILL", shortenedLength: 12 }
+  });
+  switch (event.type) {
+    case "set":
+      if (event.longUrl) {
+        throw new Error("Missing longUrl.");
+      }
+      const shortUrl = await urlShortener.shorten(event.longUrl);
+      response.body = {
+        shortUrl
+      };
+      break;
+    case "get":
+      if (event.shortUrl) {
+        throw new Error("Missing shortUrl.");
+      }
+      const longUrl = await urlShortener.getOriginalUrlFrom(event.shortUrl);
+      response.body = {
+        longUrl
+      };
+      break;
+    case "delete":
+      await urlShortener.remove(event.shortUrl);
+      break;
+    default:
+      response.statusCode = 404;
+  }
+};
--- src/setupUrlShortener.js
+++ src/setupUrlShortener.js
@@ -3,21 +3,11 @@ const mongo = require("./mongo");
 const UrlShortener = require("./UrlShortener");
 const errorMessages = require("./shortenerErrorMessages");
 
-/*
-  By default the short id will have 12 characters.
-  According to Nano ID Collision Calculator (https://zelark.github.io/nano-id-cc/) if we generate 1000 ids per hour
-  it will take ~1 thousand years to have a 1% probability of at least one collision for an id this length.
-  The shortener is capable of handling potential collisions by retrying up to 10 times.
- */
-
 async function setupUrlShortener({ dbOpts = {}, shortenerOpts = {} } = {}) {
   if (shortenerOpts.domain && !validUrl.isUri(shortenerOpts.domain)) {
     throw new Error(errorMessages.INVALID_DOMAIN);
   }
   const { collection: ShortenerCollection, rawCollection } = await mongo.init({
-    url: "mongodb://localhost:27017",
-    dbName: "shortener-test",
-    collectionName: "shortener",
     ...dbOpts
   });
   return {
