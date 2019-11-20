20d101a2433e1f82196a4e9f4b0be20bbb2407d3
Michal Mazur <mmazur@tetrascience.com>
2019-11-20 14:30:45 -0500

    remove old lambda handler

--- index.js
+++ index.js
@@ -19,42 +19,6 @@ const getUrlShortener = async () => {
   return urlShortener;
 };
 
-exports.handler = async function (event) {
-  const response = {
-    statusCode: 200,
-  };
-
-  const urlShortener = await getUrlShortener();
-
-  switch (event.type) {
-    case "set":
-      if (!event.longUrl) {
-        throw new Error("Missing longUrl.");
-      }
-      const shortUrl = await urlShortener.shorten(event.longUrl);
-      response.body = {
-        shortUrl
-      };
-      break;
-    case "get":
-      if (!event.shortUrl) {
-        throw new Error("Missing shortUrl.");
-      }
-      const longUrl = await urlShortener.getOriginalUrlFrom(event.shortUrl);
-      response.body = {
-        longUrl
-      };
-      break;
-    case "delete":
-      await urlShortener.remove(event.shortUrl);
-      break;
-    default:
-      response.statusCode = 404;
-  }
-
-  return response;
-};
-
 exports.shortenHandler = async function (event) {
   const response = {};
   const requestBody = JSON.stringify(event.body || {});
