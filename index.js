b53dd980341fd83e9164d427ee412fa087e101ea
Michal Mazur <mmazur@tetrascience.com>
2019-11-20 11:52:08 -0500

    rewrite lambda handlers for route 53 mapping integration

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const setupUrlShortener = require("./src/setupUrlShortener");
 
-exports.handler = async function(event) {
+let cachedUrlShortener = null;
+
+const getUrlShortener = async () => {
+  let urlShortener = cachedUrlShortener;
+  if (!urlShortener) {
+    ({urlShortener} = await setupUrlShortener({
+      dbOpts: {
+        url: process.env.MONGO_URL,
+        dbName: process.env.DB_NAME,
+        collectionName: process.env.COLLECTION_NAME
+      },
+      shortenerOpts: {domain: process.env.URL_PREFIX, shortenedLength: 12}
+    }));
+    cachedUrlShortener = urlShortener;
+  }
+
+  return urlShortener;
+};
+
+exports.handler = async function (event) {
   const response = {
     statusCode: 200,
   };
-  const { urlShortener } = await setupUrlShortener({
-    dbOpts: {
-      url: process.env.MONGO_URL,
-      dbName: process.env.DB_NAME,
-      collectionName: process.env.COLLECTION_NAME
-    },
-    shortenerOpts: { domain: process.env.URL_PREFIX, shortenedLength: 12 }
-  });
+
+  const urlShortener = await getUrlShortener();
+
   switch (event.type) {
     case "set":
       if (!event.longUrl) {
@@ -37,6 +51,72 @@ exports.handler = async function(event) {
     default:
       response.statusCode = 404;
   }
+
+  return response;
+};
+
+exports.shortenHandler = async function (event) {
+  const response = {};
+  const requestBody = JSON.stringify(event.body || {});
+
+  if (!requestBody.longUrl) {
+    response.statusCode = 422; // Unprocessable Entity
+    response.body = "Missing longUrl";
+  } else {
+    const urlShortener = await getUrlShortener();
+    await urlShortener.shorten(requestBody.longUrl)
+      .then(shortUrl => {
+        response.statusCode = 200;
+        response.body = shortUrl;
+      })
+      .catch(err => {
+        response.statusCode = 500;
+        response.body = err;
+      });
+  }
+
+  return response;
+};
+
+exports.retrieveHandler = async function (event) {
+  const response = {};
+  const pathParameters = event.pathParameters || {};
+  if (!pathParameters.shortId) {
+    response.statusCode = 301; // redirect to TS homepage
+    response.headers = {
+      Location: 'https://tetrascience.com'
+    }
+  } else {
+    const urlShortener = await getUrlShortener();
+    const shortUrl = `${process.env.URL_PREFIX}/${pathParameters.shortId}`;
+    await urlShortener.getOriginalUrlFrom(shortUrl)
+      .then(longUrl => {
+        response.statusCode = 301;
+        response.headers = {
+          Location: longUrl
+        };
+      })
+      .catch(err => {
+        response.statusCode = 301; // longUrl not found for the shortId provided; redirect to TS homepage
+        response.headers = {
+          Location: 'https://tetrascience.com'
+        }
+      });
+  }
+
+  return response;
+};
+
+exports.deleteHandler = async function (event) {
+  const response = {};
+
+  const urlShortener = await getUrlShortener();
+  await urlShortener.remove(event.shortUrl).then(() => {
+      response.statusCode = 200;
+  }).catch(err => {
+    response.statusCode = 500;
+    response.body = err;
+  });
 
   return response;
 };
